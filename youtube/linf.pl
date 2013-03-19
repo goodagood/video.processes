722cbe9e2ee1f03e17d4f0c48e312fd81a62b937
zatokyo <zatokyo@localhost.com>
2013-03-19 13:03:04 +0000

    to fix git conflict 0319a

---
+++ youtube/linf.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+# wget all videos listed in a file
+
+#my $vlist_filename = "/home/ubuntu/tmp/yhb";
+my $vlist_filename = "yhb";
+#my $vlist_filename = "/tmp/th";
+
+open(my $vlist, "<$vlist_filename") || die "Can not open $vlist_filename \n";
+
+my $ii = 0;
+
+while(<$vlist>){
+	#print $_ if $_ !~ /^\s*$/;
+	if( $_ !~ /^\s*$/ ){
+		my $url = $_;
+		print "$ii to get: $url \n";
+		`perl ygv.pl $url`;
+		sleep 5;
+		$ii ++;
+	}
+}
