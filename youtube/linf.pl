b2e4c2060ef7a3a7c5bde449a70be7b601e44760
Ubuntu <ubuntu@ip-10-132-174-179.ap-northeast-1.compute.internal>
2013-03-19 12:56:48 +0000

    after work move to m1.large instance in tokyo 2013 0319

--- youtube/linf.pl
+++ youtube/linf.pl
@@ -2,17 +2,21 @@
 
 # wget all videos listed in a file
 
+#my $vlist_filename = "/home/ubuntu/tmp/yhb";
 my $vlist_filename = "yhb";
 #my $vlist_filename = "/tmp/th";
 
 open(my $vlist, "<$vlist_filename") || die "Can not open $vlist_filename \n";
 
+my $ii = 0;
+
 while(<$vlist>){
 	#print $_ if $_ !~ /^\s*$/;
 	if( $_ !~ /^\s*$/ ){
 		my $url = $_;
-		print "\n to get: $url \n";
+		print "$ii to get: $url \n";
 		`perl ygv.pl $url`;
 		sleep 5;
+		$ii ++;
 	}
 }
---
+++ youtube/youtubelist.pl
@@ -0,0 +1,6 @@
+
+`wget $ARGV[0] -O /tmp/youtubeplaylista.html`;
+
+`grep -o -P '\/watch[^&]+'   /tmp/youtubeplaylista.html  > /tmp/ya`;
+
+
